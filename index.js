adba7012f37bdc9ceaef3ab2990627af5502370f
Matt Orndorff <ornmatt321@gmail.com>
2022-02-10 14:27:02 -0500

    MVPing still

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    return {name, price, category}
 }
-
+console.log(createMenuItem('tacos', 8, 'Lunch'))
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
 Test your createMenuItems function by doing the following:
@@ -28,8 +28,9 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
-
-
+console.log(createMenuItem('pizza', 5, 'Lunch'));
+console.log(createMenuItem('potpie', 7, 'Dinner'));
+console.log(createMenuItem('grits', 4, 'Breakfast'))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the 
@@ -48,8 +49,17 @@ const burger = {
   price: 18, 
   category: "Lunch", 
   
+  discount: function(string){
+    if(string === 'teacher' || string === 'student'){
+      return this.price - (this.price * .25);
+    }else if(string === 'public'){
+      return this.price - (this.price * .10)
+    }else{
+      return 'no soup for you!'
+    }
+  }
 }
-
+console.log(burger.discount('student'));
 
 
 ///////////////Reviews (MVP)///////////////////
